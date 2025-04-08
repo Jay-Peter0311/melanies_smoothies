5a28bbed5953b8c5850243137f3be5f33f5a9777
Jay-Peter0311 <jaypeter0311@gmail.com>
2025-04-08 14:48:11 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
-pd_pf=my_dataframe.to_pandas()
+pd_df=my_dataframe.to_pandas()
 
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
